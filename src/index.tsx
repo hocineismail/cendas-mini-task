0601589127ee1f8fa88cfaa33185098848452fc7
hocineismail <hocineismail75@gmail.com>
2024-02-22 00:38:13 +0100

    FIXBUG: use workbox for offline

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,10 @@ import App from "./App";
 //impliment redux for state management
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { register } from "./register";
 
 const rootElement = document.getElementById("root") as HTMLElement;
+// import * as serviceWorker from "./serviceWorker";
 
 const root = createRoot(rootElement);
 
@@ -15,19 +17,19 @@ root.render(
     <App />
   </Provider>
 );
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js") // Path to your service worker file
-      .then((registration) => {
-        console.log(
-          "Service Worker registered with scope:",
-          registration.scope
-        );
-      })
-      .catch((err) => {
-        console.error("Service Worker registration failed:", err);
-      });
-  });
-}
+register();
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", () => {
+//     navigator.serviceWorker
+//       .register("/service-worker.js") // Path to your service worker file
+//       .then((registration) => {
+//         console.log(
+//           "Service Worker registered with scope:",
+//           registration.scope
+//         );
+//       })
+//       .catch((err) => {
+//         console.error("Service Worker registration failed:", err);
+//       });
+//   });
+// }
