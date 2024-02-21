fddaf2ca98f5ca8aac860219b5df8bc7614470f3
hocineismail <hocineismail75@gmail.com>
2024-02-21 21:33:45 +0100

    Added comments for clarity and addressed specific concerns.

--- src/redux/actions/tasksActions.ts
+++ src/redux/actions/tasksActions.ts
@@ -1,58 +1,65 @@
-
+// Import necessary dependencies
 import getDatabase from "@db/database";
-
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { v4 as uuidv4 } from 'uuid';
 import { ITask, IChecklist, ITodo, IUpdateStatus, TaskTarget, ChecklistTarget, ItemTarget } from '@type/types';
 import { ChecklistDocType, ItemDocType } from "@type/schema";
 
-
-
-
-
-// Define the async thunk
+// Define the async thunk to fetch tasks
 const fetchTasksAsync = createAsyncThunk('tasks/fetchTasks', async (_, thunkAPI) => {
-    const currentState = await thunkAPI.getState();
-    console.log(currentState)
-    const db = await getDatabase();
-    const currentUser = await db.users.findOne({
-        selector: {
-            username: localStorage.getItem("username")
-        }
-    }).exec();
+    try {
+        // Access the current state
+        const currentState = await thunkAPI.getState();
+        console.log(currentState);
 
-    if (!currentUser) {
-        throw new Error('User not Found');
-    }
-    const tasks = await db.tasks.find().where("user").sort({ createdAt: "desc" }).eq(currentUser._id).exec();
-    const taskPromise = tasks.map(async task => {
+        // Retrieve the database instance
+        const db = await getDatabase();
 
-        const checklists = await db.checklists.find().where("task").eq(task._id).exec();
-        const checkiltsPrimise = checklists.map(async item => {
-            const items = await db.items.find().where("checklist").sort({ createdAt: "asc" }).eq(item._id).exec()
-            const result = {
-                ...item._data,
-                items: items.map((item) => item._data)
+        // Find the current user based on the username stored in local storage
+        const currentUser = await db.users.findOne({
+            selector: {
+                username: localStorage.getItem("username")
             }
-            return result
-        });
-        const checklistsWithItem = await Promise.all(checkiltsPrimise);
-        const result = {
-            ...task._data,
-            checklists: checklistsWithItem
-        }
-        return result
-    });
-    const usersWithTasks = await Promise.all(taskPromise);
-    return usersWithTasks;
+        }).exec();
 
+        if (!currentUser) {
+            throw new Error('User not found');
+        }
+
+        // Retrieve tasks associated with the current user
+        const tasks = await db.tasks.find().where("user").sort({ createdAt: "desc" }).eq(currentUser._id).exec();
+
+        // Fetch additional details for each task, including checklists and items
+        const taskPromise = tasks.map(async task => {
+            const checklists = await db.checklists.find().where("task").eq(task._id).exec();
+            const checklistPromise = checklists.map(async item => {
+                const items = await db.items.find().where("checklist").sort({ createdAt: "asc" }).eq(item._id).exec();
+                const result = {
+                    ...item._data,
+                    items: items.map((item) => item._data)
+                };
+                return result;
+            });
+            const checklistsWithItems = await Promise.all(checklistPromise);
+            const result = {
+                ...task._data,
+                checklists: checklistsWithItems
+            };
+            return result;
+        });
+
+        const usersWithTasks = await Promise.all(taskPromise);
+        return usersWithTasks;
+    } catch (error) {
+        console.error(error);
+        throw new Error('Failed to fetch tasks');
+    }
 });
 
-
-// Create an asynchronous thunk for adding a task
+// Define the async thunk to add a task
 const addTaskAsync = createAsyncThunk('tasks/addTask', async ({ title, description, selectedColor }: ITask) => {
     try {
-        // Get the database instance
+        // Retrieve the database instance
         const db = await getDatabase();
 
         // Find the user in the database based on the username stored in local storage
@@ -86,16 +93,15 @@ const addTaskAsync = createAsyncThunk('tasks/addTask', async ({ title, descripti
             checklists: []
         };
     } catch (error) {
-        // Log and throw an error if there's an issue with the database operation
-        console.log(error);
-        throw new Error('Failed to fetch tasks');
+        console.error(error);
+        throw new Error('Failed to add task');
     }
 });
 
-// Create an asynchronous thunk for adding a checklist
+// Define the async thunk to add a checklist
 const addChecklistAsync = createAsyncThunk('tasks/addChecklist', async ({ task_id, title }: IChecklist) => {
     try {
-        // Find the current task in the database
+        // Retrieve the database instance
         const db = await getDatabase();
 
         // Create a new checklist object
@@ -110,22 +116,22 @@ const addChecklistAsync = createAsyncThunk('tasks/addChecklist', async ({ task_i
         // Insert the new checklist into the database
         const checklist = await db.checklists.insert(newChecklist);
 
-        // Return the checklist data along with an empty items array to avoid error on item map()
+        // Return the checklist data along with an empty items array
         return {
             ...checklist,
             items: []
         };
 
     } catch (error) {
-        // Throw an error if there's an issue with the database operation
-        return new Error('Failed to fetch newChecklists');
+        console.error(error);
+        throw new Error('Failed to add checklist');
     }
 });
 
-// Create an asynchronous thunk for adding an item
+// Define the async thunk to add an item
 const addItemAsync = createAsyncThunk('tasks/addItem', async ({ checklist_id, emoji, title, description, selectedColor, task_id }: ITodo) => {
     try {
-        // Find the current task in the database
+        // Retrieve the database instance
         const db = await getDatabase();
 
         // Create a new item object
@@ -150,15 +156,15 @@ const addItemAsync = createAsyncThunk('tasks/addItem', async ({ checklist_id, em
         };
 
     } catch (error) {
-        // Throw an error if there's an issue with the database operation
-        throw new Error('Failed to fetch newChecklists');
+        console.error(error);
+        throw new Error('Failed to add item');
     }
 });
 
-// Create an asynchronous thunk for updating an item's status
+// Define the async thunk to update an item's status
 const updateItemStatusAsync = createAsyncThunk('tasks/updateItemStatus', async ({ checklist_id, task_id, item_id }: IUpdateStatus) => {
     try {
-        // Find the current task in the database
+        // Retrieve the database instance
         const db = await getDatabase();
 
         // Find the target item based on the item_id
@@ -181,72 +187,78 @@ const updateItemStatusAsync = createAsyncThunk('tasks/updateItemStatus', async (
         };
 
     } catch (error) {
-        // Throw an error if there's an issue with the database operation
-        throw new Error('Failed to fetch newChecklists');
+        console.error(error);
+        throw new Error('Failed to update item status');
     }
 });
 
-
-
-// Create an asynchronous thunk for updating an item's status
+// Define the async thunk to delete a task
 const deleteTaskAsync = createAsyncThunk('tasks/deleteTask', async ({ task_id }: TaskTarget) => {
     try {
-        // Find the current task in the database
+        // Retrieve the database instance
         const db = await getDatabase();
 
-        // Find the target item based on the item_id
+        // Remove the target task from the database
         await db.tasks.findOne().where("_id").eq(task_id).remove();
+
+        // Remove associated checklists and items
         const checklists = await db.checklists.find().where("task").eq(task_id).remove();
         checklists.forEach(async (checklist: ChecklistDocType) => {
-            const checklist_id = checklist._id
+            const checklist_id = checklist._id;
             await db.items.find().where("checklist").eq(checklist_id).remove();
+        });
 
-        })
-        return task_id
+        return task_id;
     } catch (error) {
-        // Throw an error if there's an issue with the database operation
-        throw new Error('Failed to fetch newChecklists');
+        console.error(error);
+        throw new Error('Failed to delete task');
     }
 });
 
 
 
+
+// Define the async thunk to delete a checklist
 const deleteChecklistAsync = createAsyncThunk('tasks/deleteChecklist', async ({ checklist_id }: ChecklistTarget) => {
     try {
-        // Find the current task in the database
+        // Retrieve the database instance
         const db = await getDatabase();
 
-        // Find the target item based on the item_id
+        // Find the target checklist based on the checklist_id and remove it
         const checklist = await db.checklists.findOne().where("_id").eq(checklist_id).remove();
+
+        // Remove associated items for the checklist
         await db.items.find().where("checklist").eq(checklist_id).remove();
 
+        // Return the checklist_id and associated task_id
         return {
             checklist_id,
             task_id: checklist.task
-        }
+        };
     } catch (error) {
         // Throw an error if there's an issue with the database operation
-        throw new Error('Failed to fetch newChecklists');
+        throw new Error('Failed to delete checklist');
     }
 });
+
+// Define the async thunk to delete an item
 const deleteItemAsync = createAsyncThunk('tasks/deleteItem', async ({ checklist_id, task_id, item_id }: ItemTarget) => {
     try {
-        // Find the current task in the database
+        // Retrieve the database instance
         const db = await getDatabase();
 
-        // Find the target item based on the item_id
+        // Find the target item based on the item_id and remove it
         await db.items.findOne().where("_id").eq(item_id).remove();
 
-
+        // Return checklist_id, task_id, and item_id
         return {
             checklist_id, task_id, item_id
-        }
+        };
     } catch (error) {
         // Throw an error if there's an issue with the database operation
-        throw new Error('Failed to fetch newChecklists');
+        throw new Error('Failed to delete item');
     }
 });
-
 export {
     deleteItemAsync, fetchTasksAsync, addTaskAsync, addChecklistAsync, addItemAsync, updateItemStatusAsync, deleteTaskAsync, deleteChecklistAsync
 }
